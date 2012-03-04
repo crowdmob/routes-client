38e0e43cba3803b4bcc2cc0e69a657fda3961d6c
Matthew Moore <matt@crowdmob.com>
2012-03-04 11:17:07 -0800

    url helpers

--- lib/routes-client.rb
+++ lib/routes-client.rb
@@ -2,6 +2,24 @@ require "routes-client/version"
 
 module Routes
   module Client
-    # Your code goes here...
+    
+    module Controllers
+      autoload :UrlHelpers, 'routes-client/controllers/url_helpers'
+    end
+    
+    # Host to lookup routes
+    SERVER = 'http://cm-routes.herokuapp.com'
+    
+    URL_HELPERS = ActiveSupport::OrderedHash.new
+    
+    # The router Routes::Client should use to generate routes. Defaults
+    # to :main_app. Should be overriden by engines in order
+    # to provide custom routes.
+    mattr_accessor :router_name
+    @@router_name = nil
+    
+    def self.available_router_name
+      router_name || :main_app
+    end
   end
 end
---
+++ lib/routes-client/controllers/url_helpers.rb
@@ -0,0 +1,48 @@
+module Routes
+  module Client
+    module Controllers
+      # Create url helpers for cross-application linking in different envss.
+      # Calls http://cm-routes.herokuapp.com/[environment].json and creates.
+      # all the url helpers from it.
+      #
+      # Those helpers are included by default to ActionController::Base.
+      #
+      # In case you want to add such helpers to another class, you can do
+      # that as long as this new class includes both url_helpers and
+      # mounted_helpers. Example:
+      #
+      #     include Rails.application.routes.url_helpers
+      #     include Rails.application.routes.mounted_helpers
+      #
+      # Run rake routes to see what it picked up (for your current environment)
+      #
+      module UrlHelpers
+        def self.remove_helpers!
+          self.instance_methods.map(&:to_s).grep(/_(url|path)$/).each do |method|
+            remove_method method
+          end
+        end
+
+        def self.generate_helpers!(routes=nil)
+          routes ||= begin
+            require 'open-uri'
+            mappings = ActiveSupport::JSON.decode(open(Routes::Client::SERVER) + "/#{ENV["RAILS_ENV"]}.json")
+            Routes::Client::URL_HELPERS.slice(*mappings)
+          end
+
+          routes.each do |app_name, host|
+            method = "crowdmob_#{app_name}_url"
+
+            class_eval <<-URL_HELPERS, __FILE__, __LINE__ + 1
+              def #{method}(*args)
+                Routes::Client::URL_HELPERS[app_name] + "/"
+              end
+            URL_HELPERS
+          end
+        end
+
+        generate_helpers!(Routes::Client::URL_HELPERS)
+      end
+    end
+  end
+end
